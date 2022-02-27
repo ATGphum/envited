12f9d81dd97d1555c8d87576ff8a4b117ba175ab
PhumLWC <jeremyvuong.dshs@gmail.com>
2022-02-27 14:43:26 +0800

    wip: got basic ui down

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,28 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/homepage';
 import About from './pages/about';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Global } from "@emotion/react"
+import { extendTheme } from "@chakra-ui/react"
+
+
+
+const theme = extendTheme({
+  fonts: {
+    heading: "Open Sans, sans-serif",
+    body: "Open Sans, sans-serif",
+  },
+})
 
 function App() {
   return (
-    <div className="App">
-     <Routes>
-        <Route path="/About" element={<About />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </div>
+    <ChakraProvider theme={theme}>
+        <Routes>
+          <Route path="/create" element={<About />} />
+          <Route path="/event" element={<About />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+    </ChakraProvider>
   );
 }
 
--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,3 +1,4 @@
+import { Box, Flex, Text } from "@chakra-ui/react";
 import React from "react"
 import { useNavigate } from 'react-router-dom';
 import styles from "./homepage.module.css"
@@ -7,10 +8,27 @@ const Home: React.FC = () => {
     const navigate = useNavigate()
 
     return (
-        <div className={styles.page}>
-        <div className={styles.headerBar} onClick={() => navigate('/about')}>poopy</div>
-        </div>
-    )
+      <Flex className={styles.page}>
+        <Flex className={styles.topBar}>
+          <Flex>
+            <Text className={styles.logoTitle}>envited</Text>
+          </Flex>
+          <Flex className={styles.logInWrapper}>
+            <Text className={styles.logIn}>Log in</Text>
+          </Flex>
+        </Flex>
+        <Flex className={styles.middleWrapper}>
+          <Text className={styles.pageHeader}>
+            Facebook events without Facebook.
+          </Text>
+          <Text className={styles.pageDescription}>
+            Easily host and share events with your friends across any social
+            media.
+          </Text>
+          <Box className={styles.pitchWrapper}><Text className={styles.pitch}>&#127881; Create my next event</Text></Box>
+        </Flex>
+      </Flex>
+    );
 }
 
 export default Home;
